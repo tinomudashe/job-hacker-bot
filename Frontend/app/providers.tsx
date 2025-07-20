309d4baffd402e3d1b633b1b517b5f30a517397b
TINOMUDASHE HAPSON MARECHA <jnrhapson@yahoo.com>
2025-07-20 22:06:51 +0200

    editted a provider

--- Frontend/app/providers.tsx
+++ Frontend/app/providers.tsx
@@ -1,16 +1,27 @@
 "use client";
 
-import { PageProvider } from "@/lib/hooks/use-page-context";
-import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { ThemeProvider } from "next-themes";
 
-export function Providers({ children, ...props }: ThemeProviderProps) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <NextThemesProvider {...props}>
-        <PageProvider>{children}</PageProvider>
-      </NextThemesProvider>
-    </ClerkProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <header className="p-4 flex justify-end items-center gap-4">
+        <ThemeToggle />
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+      <main>{children}</main>
+    </ThemeProvider>
   );
 }
