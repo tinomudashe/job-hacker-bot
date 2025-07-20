8cba1cb42eca1910091b2cc1ffdccba86f1e047d
TINOMUDASHE HAPSON MARECHA <jnrhapson@yahoo.com>
2025-07-21 01:23:47 +0200

    Globalcss fix

--- Frontend/tailwind.config.ts
+++ Frontend/tailwind.config.ts
@@ -5,10 +5,8 @@ import tailwindcssAnimate from "tailwindcss-animate";
 const config = {
   darkMode: "class",
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   prefix: "",
   theme: {
