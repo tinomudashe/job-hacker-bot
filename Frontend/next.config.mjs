114c658f9520e581495afa83a473f8734931d411
TINOMUDASHE HAPSON MARECHA <jnrhapson@yahoo.com>
2025-07-21 21:16:36 +0200

    fixing routing destination

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -70,7 +70,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:8000/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
   },
